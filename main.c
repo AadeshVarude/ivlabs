48a5e80ff4c0c0572c38c1655fad98a9abfcc597
AadeshVarude <50541542+AadeshVarude@users.noreply.github.com>
2019-05-12 14:06:22 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define int u =0;
+int main()
+{   int h=0,s=0;
+    int m,n,i,max,j;
+    printf("enter the value of n");
+    int len[m],price[m];
+    printf("enter the price and the lengths")
+    for(i=0,i<m,i++)
+        {
+            scanf(%d,&len[i]);
+            scanf(%d,&price[i]);
+        }
+     for(i=0,i<m,i++)
+     {
+         for(k=0,k<m,k++)
+            {   for(j=0,j<m,j++)
+                {  a = h+len[j];
+                    if(a==n)
+                    {
+                        max = s+price[i] ;
+                    }
+                    h=func();
+                }
+                u = len[k];
+            }
+
+
+     }
+
+    return 0;
+}
+func()
+{
+    int x=0,h;
+    h= u + len[x];
+    s= price[x];
+    x++;
+    return h,s;
+}
